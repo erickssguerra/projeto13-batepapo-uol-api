33ee7e6b89c5eb5a83118da230aa336084b62de8
erickssguerra <erickssguerra@gmail.com>
2022-11-10 12:50:08 -0300

    MongoClient connection + Creation of dbBatepapoUOL database + Creation of participants collection inside dbBatepapouol + GET participants from dbBatepapoUOL

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-import express from "express"
+import express, { json } from "express"
 import cors from "cors"
+import dotvenv from "dotenv"
+import { MongoClient } from "mongodb"
 
+// configs
+dotvenv.config()
+const mongoClient = new MongoClient(process.env.MONGO_URI)
+await mongoClient.connect()
+const dbBatepapoUOL = mongoClient.db("batepapo-uol-api")
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(json())
 
+app.get("/participants", async (req, res) => {
+    try {
+        const participants = await dbBatepapoUOL.collection("participants").find({}).toArray()
+        res.send(participants)
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
 
+// connection
 app.listen(5000, () => {
     console.log("You're connect to port 5000!")
 })
