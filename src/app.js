ec659e303f74c4dd38727f812f4796ec0bbdbf08
erickssguerra <erickssguerra@gmail.com>
2022-11-11 01:35:43 -0300

    feat: validation of 'to' and 'text' in POST /messages

--- src/app.js
+++ src/app.js
@@ -28,14 +28,19 @@ const participantSchema = joi.object({
     name: joi.string().required()
 })
 
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required()
+})
+
 // route participants
 server.post("/participants", async (req, res) => {
-    const name = req.body.name
+    const { name } = req.body
 
     const validation = participantSchema.validate({ name }, { abortEarly: false })
     if (validation.error) {
-        const message = validation.error.details.map((detail) => detail.message)
-        res.status(422).send(message)
+        const errorMessage = validation.error.details.map((detail) => detail.message)
+        res.status(422).send(errorMessage)
         return
     }
 
@@ -83,6 +88,19 @@ server.get("/messages", async (req, res) => {
         console.log(err)
         res.sendStatus(500)
     }
+})
+
+server.post("/messages", async (req, res) => {
+    const { to, text, type } = req.body
+    const from = req.headers.user
+
+    const validation = messageSchema.validate({ to, text }, { abortEarly: false })
+    if (validation.error) {
+        const errorMessage = validation.error.details.map((detail) => detail.message)
+        res.status(422).send(errorMessage)
+        return
+    }
+    
 })
 
 // connection
